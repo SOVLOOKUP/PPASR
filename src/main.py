ba609890464ab8fe21fdbdeaca6716bc542fc9f8
sovlookup <gonorth@qq.com>
2022-07-06 13:38:41 +0800

    init

---
+++ src/main.py
@@ -0,0 +1,23 @@
+from paddlespeech.server.bin.paddlespeech_server import ServerExecutor
+import threading
+
+streaming_asr_server = ServerExecutor()
+streaming_punc_server = ServerExecutor()
+
+asr = threading.Thread(target=streaming_asr_server,
+                       kwargs={
+                           "config_file": "conf/ws_conformer_wenetspeech_application.yaml",
+                           "log_file": "log/paddlespeech_asr.log"
+                       })
+
+punc = threading.Thread(target=streaming_punc_server,
+                        kwargs={
+                            "config_file": "conf/punc_application.yaml",
+                            "log_file": "log/paddlespeech_punc.log"
+                        })
+
+if __name__ == "__main__":
+    asr.start()
+    punc.start()
+    asr.join()
+    punc.join()
